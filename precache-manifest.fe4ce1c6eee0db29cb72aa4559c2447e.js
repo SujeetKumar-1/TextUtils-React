fef2752b5cdabef1c42a9cebf7656256ba0d212d
golwall <sujeetkumar01357@gmail.com>
2024-07-24 00:46:40 +0530

    Updates

---
+++ precache-manifest.fe4ce1c6eee0db29cb72aa4559c2447e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c89c339e24060744e0ffb9ac1ed05d",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "5a46d9d2bdb667b7f25c",
+    "url": "/TextUtils-React/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "8ea57846da87c05e2447",
+    "url": "/TextUtils-React/static/js/2.f8a3d5ae.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils-React/static/js/2.f8a3d5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/TextUtils-React/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "5a46d9d2bdb667b7f25c",
+    "url": "/TextUtils-React/static/js/main.d422b2fa.chunk.js"
+  },
+  {
+    "revision": "6e787fb2f9a58407d883",
+    "url": "/TextUtils-React/static/js/runtime-main.994c20f5.js"
+  }
+]);
